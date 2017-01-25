a67547db4882fbf9eeb7eeed9cd1cd2a086511d1
Colin Wood <cwood06@gmail.com>
2017-01-25 13:49:41 -0800

    Migrate stats to signal file for stat resources

--- organization.go
+++ organization.go
@@ -2,25 +2,13 @@ package sentry
 
 import (
 	"fmt"
-	"net/url"
-	"strconv"
 	"time"
 )
 
 const (
-
 	// OrgEndpointName is set to roganizations
 	OrgEndpointName = "organizations"
-	// StatReceived is set to received for sending to /stats/ endpoints
-	StatReceived StatQuery = "received"
-	// StatRejected is set to rejected for sending to /stats/ endpoints
-	StatRejected StatQuery = "rejected"
-	// StatBlacklisted is set to blacklisted for sending to /stats/ endpoints
-	StatBlacklisted StatQuery = "blacklisted"
 )
-
-// StatQuery is semple type for sending to /stats/ endpoints
-type StatQuery string
 
 // Quota is your quote for a project limit and max rate
 type Quota struct {
@@ -39,43 +27,6 @@ type Organization struct {
 	ID                   *string    `json:"id,omitempty"`
 	IsEarlyAdopter       *bool      `json:"isEarlyAdopter,omitempty"`
 	Features             *[]string  `json:"features,omitempty"`
-}
-
-type orgStatRequest struct {
-	Stat       StatQuery `json:"stat"`
-	Since      int64     `json:"since"`
-	Until      int64     `json:"until"`
-	Resolution string    `json:"resolution,omitempty"`
-}
-
-func (o *orgStatRequest) ToQueryString() string {
-	query := url.Values{}
-	query.Add("stat", string(o.Stat))
-	query.Add("since", strconv.FormatInt(o.Since, 10))
-	query.Add("until", strconv.FormatInt(o.Until, 10))
-	return query.Encode()
-}
-
-// OrganizationStat is used for tetting a time in seconds and the metric in a float
-type OrganizationStat [2]float64
-
-// GetOrganizationStats fetches stats from the org. Needs a Organization, a StatQuery, a timestamp in seconds since epoch and a optional resolution
-func (c *Client) GetOrganizationStats(org Organization, stat StatQuery, since, until int64, resolution *string) ([]OrganizationStat, error) {
-	var orgstats []OrganizationStat
-
-	optionalResolution := ""
-	if resolution != nil {
-		optionalResolution = *resolution
-	}
-	orgstatrequest := &orgStatRequest{
-		Stat:       stat,
-		Since:      since,
-		Until:      until,
-		Resolution: optionalResolution,
-	}
-
-	err := c.do("GET", fmt.Sprintf("%s/%s/stats", OrgEndpointName, *org.Slug), &orgstats, orgstatrequest)
-	return orgstats, err
 }
 
 // GetOrganization takes a org slug and returns back the org
@@ -111,4 +62,11 @@ func (c *Client) UpdateOrganization(org Organization) error {
 // DeleteOrganization will delete the Org. There is not way to revert this if you do.
 func (c *Client) DeleteOrganization(org Organization) error {
 	return c.do("DELETE", fmt.Sprintf("%s/%s", OrgEndpointName, *org.Slug), nil, nil)
+}
+
+// GetOrganizationTeams will fetch all teams for this org
+func (c *Client) GetOrganizationTeams(o Organization) ([]Team, error) {
+	teams := make([]Team, 0)
+	err := c.do("GET", fmt.Sprintf("organizations/%s/teams", *o.Slug), &teams, nil)
+	return teams, err
 }
---
+++ stat.go
@@ -0,0 +1,62 @@
+package sentry
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+)
+
+const (
+	// StatReceived is set to received for sending to /stats/ endpoints
+	StatReceived StatQuery = "received"
+	// StatRejected is set to rejected for sending to /stats/ endpoints
+	StatRejected StatQuery = "rejected"
+	// StatBlacklisted is set to blacklisted for sending to /stats/ endpoints
+	StatBlacklisted StatQuery = "blacklisted"
+)
+
+type statRequest struct {
+	Stat       StatQuery `json:"stat"`
+	Since      int64     `json:"since"`
+	Until      int64     `json:"until"`
+	Resolution string    `json:"resolution,omitempty"`
+}
+
+func (o *statRequest) ToQueryString() string {
+	query := url.Values{}
+	query.Add("stat", string(o.Stat))
+	query.Add("since", strconv.FormatInt(o.Since, 10))
+	query.Add("until", strconv.FormatInt(o.Until, 10))
+	return query.Encode()
+}
+
+// Stat is used for tetting a time in seconds and the metric in a float
+type Stat [2]float64
+
+// StatQuery is sample type for sending to /stats/ endpoints
+type StatQuery string
+
+// GetOrganizationStats fetches stats from the org. Needs a Organization, a StatQuery, a timestamp in seconds since epoch and a optional resolution
+func (c *Client) GetOrganizationStats(org Organization, stat StatQuery, since, until int64, resolution *string) ([]Stat, error) {
+	var orgstats []Stat
+
+	optionalResolution := ""
+	if resolution != nil {
+		optionalResolution = *resolution
+	}
+	orgstatrequest := &statRequest{
+		Stat:       stat,
+		Since:      since,
+		Until:      until,
+		Resolution: optionalResolution,
+	}
+
+	err := c.do("GET", fmt.Sprintf("%s/%s/stats", OrgEndpointName, *org.Slug), &orgstats, orgstatrequest)
+	return orgstats, err
+}
+
+// GetTeamStats will fetch all stats for a specific team. Similar to GetOrganizationStats
+func (c *Client) GetTeamStats(o Organization, t Team, stat StatQuery, since, until int64, resolution *string) ([]Stat, error) {
+	teamstats := make([]Stat, 0)
+	return teamstats, nil
+}
