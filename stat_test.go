90b6b8d4f4404d8b75a6a37ca9205c49d79ead6d
Colin Wood <cwood06@gmail.com>
2017-01-25 14:03:24 -0800

    Add test for stat

---
+++ stat_test.go
@@ -0,0 +1,47 @@
+package sentry
+
+import (
+	"testing"
+	"time"
+)
+
+func TestStat(t *testing.T) {
+	now := time.Now().Unix()
+	hourlater := time.Duration(1) * time.Hour
+	later := now - int64(hourlater.Seconds())
+
+	org, err := client.GetOrganization("sentry")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Run Getting Organization Stats", func(t *testing.T) {
+		if err != nil {
+			t.Fatal(err)
+		}
+		stats, err := client.GetOrganizationStats(org, StatReceived, later, now, nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if len(stats) <= 0 {
+			t.Error("No stats were returned")
+		}
+	})
+	t.Run("Run Getting Team Stats", func(t *testing.T) {
+		team, err := client.CreateTeam(org, "Test Team for Stats", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = client.GetTeamStats(org, team, StatReceived, later, now, nil)
+		if err != nil {
+			t.Error(err)
+		}
+
+		err = client.DeleteTeam(org, team)
+		if err != nil {
+			t.Error("Failed to delete test team")
+		}
+	})
+
+}
