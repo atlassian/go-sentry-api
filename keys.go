206192cfbcace2f1512775695b1a121b2ed4bce5
Colin Wood <cwood06@gmail.com>
2017-01-25 17:15:05 -0800

    Create/update/delete keys with tests

---
+++ keys.go
@@ -0,0 +1,52 @@
+package sentry
+
+import (
+	"fmt"
+	"time"
+)
+
+// DSN is the actual connection strings used to send errors
+type DSN struct {
+	Secret string `json:"secret,omitempty"`
+	CSP    string `json:"csp,omitempty"`
+	Public string `json:"public,omitempty"`
+}
+
+// Key is a DSN that sentry has made
+type Key struct {
+	Label       string    `json:"label,omitempty"`
+	DSN         DSN       `json:"dsn,omitempty"`
+	Secret      string    `json:"secret,omitempty"`
+	ID          string    `json:"id,omitempty"`
+	DateCreated time.Time `json:"dateCreated,omitempty"`
+	Public      string    `json:"public,omitempty"`
+}
+
+type nameReq struct {
+	Name string `json:"name"`
+}
+
+//CreateClientKey creates a new client key for a project and org
+func (c *Client) CreateClientKey(o Organization, p Project, name string) (Key, error) {
+	var key Key
+	req := &nameReq{
+		Name: name,
+	}
+	err := c.do("POST", fmt.Sprintf("projects/%s/%s/keys", *o.Slug, *p.Slug), &key, &req)
+	return key, err
+}
+
+//DeleteClientKey deletes a client key for a project and org
+func (c *Client) DeleteClientKey(o Organization, p Project, k Key) error {
+	return c.do("DELETE", fmt.Sprintf("projects/%s/%s/keys/%s", *o.Slug, *p.Slug, k.ID), nil, nil)
+}
+
+//UpdateClientKey updates the name only of a key
+func (c *Client) UpdateClientKey(o Organization, p Project, k Key, name string) (Key, error) {
+	var key Key
+	req := &nameReq{
+		Name: name,
+	}
+	err := c.do("PUT", fmt.Sprintf("projects/%s/%s/keys/%s", *o.Slug, *p.Slug, k.ID), &key, &req)
+	return key, err
+}
---
+++ keys_test.go
@@ -0,0 +1,53 @@
+package sentry
+
+import (
+	"testing"
+)
+
+func TestKeysResource(t *testing.T) {
+	org, err := client.GetOrganization("sentry")
+	if err != nil {
+		t.Fatal(err)
+	}
+	team, err := client.CreateTeam(org, "test team for keys", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	project, err := client.CreateProject(org, team, "Test python project keys", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Create a new key for project", func(t *testing.T) {
+		key, err := client.CreateClientKey(org, project, "Test client key")
+		if err != nil {
+			t.Error(err)
+		}
+		if key.Label != "Test client key" {
+			t.Error("Key does not have correct label")
+		}
+		t.Run("Update name of client key", func(t *testing.T) {
+
+			key, err = client.UpdateClientKey(org, project, key, "This is a new name")
+			if err != nil {
+				t.Error(err)
+			}
+
+			if key.Label != "This is a new name" {
+				t.Error("Failed to update to a new name")
+			}
+
+			t.Run("Deleting the client key", func(t *testing.T) {
+				err := client.DeleteClientKey(org, project, key)
+				if err != nil {
+					t.Error(err)
+				}
+			})
+		})
+	})
+
+	if err := client.DeleteTeam(org, team); err != nil {
+		t.Error(err)
+	}
+
+}
