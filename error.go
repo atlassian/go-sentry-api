16d98452340c8ce272612c9dcf748a5ff7d660fc
Colin Wood <cwood06@gmail.com>
2017-01-31 13:42:52 -0800

    Change message for 400s that dont have a detail string

--- error.go
+++ error.go
@@ -15,5 +15,8 @@ func (s APIError) Error() string {
 	if s.StatusCode == 404 {
 		return "404: Endpoint/Resource not found"
 	}
+	if s.StatusCode == 400 && s.Detail == "" {
+		return fmt.Sprintf("400: Bad Request. Invalid data is probably the issue")
+	}
 	return fmt.Sprintf("%d: %s", s.StatusCode, s.Detail)
 }
