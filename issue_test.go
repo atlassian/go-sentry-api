80d3642b8a5e8814279d59edf6b8cb6c461b0edd
Adeline Yeung <ayeung@atlassian.com>
2020-12-15 17:23:27 +1100

    Miscellaneous changes - change makefile kill commands to not nuke everything, remove extraneous punctuation

--- issue_test.go
+++ issue_test.go
@@ -83,7 +83,7 @@ func TestIssueResource(t *testing.T) {
 			}
 			for _, issue := range issues {
 				if issue.Stats.FourteenDays == nil {
-					t.Fatal("We should be able to get 14 days of stats for this issue but didn't.")
+					t.Fatal("We should be able to get 14 days of stats for this issue but didn't")
 				}
 			}
 		})
@@ -141,11 +141,11 @@ func TestIssueResource(t *testing.T) {
 
 			details, ok := (*firstIssue.StatusDetails)["inNextRelease"].(bool)
 			if !ok {
-				t.Error("Status did not get updated")
+				t.Error("Status details did not get updated")
 			}
 
 			if !details {
-				t.Error("Status did not get updated")
+				t.Error("Status details did not get updated")
 			}
 
 			t.Run("Delete the first issue in this project", func(t *testing.T) {
